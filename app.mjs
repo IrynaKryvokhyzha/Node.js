766d842cf52b2e98c6ccbf92be19ea92a90b9c7e
Iryna <irynazankovska@gmail.com>
2024-11-29 18:40:44 -0500

    added CRUD for students

--- app.mjs
+++ app.mjs
@@ -1,13 +1,18 @@
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
+import connectDB from "./db/connectDB.mjs";
 import logger from "morgan";
 import { fileURLToPath } from "url";
 import indexRouter from "./routes/index.mjs";
 import usersRouter from "./routes/users.mjs";
+import studentsRouter from "./routes/students.mjs";
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
+connectDB();
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("dev"));
@@ -17,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/students", studentsRouter);
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error("Not Found");
--- routes/index.mjs
+++ routes/index.mjs
@@ -1,6 +1,6 @@
 import { Router } from "express";
 const router = Router();
 router.get("/", (req, res) => {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Задача 2." });
 });
 export default router;
