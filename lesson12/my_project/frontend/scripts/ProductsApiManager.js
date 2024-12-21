3631924d905708df4f99e66c4d91a5f76972aba7
Iryna <irynazankovska@gmail.com>
2024-12-21 16:35:07 -0500

    added sort to products

--- lesson12/my_project/frontend/scripts/ProductsApiManager.js
+++ lesson12/my_project/frontend/scripts/ProductsApiManager.js
@@ -1,22 +1,7 @@
-class ProductsApiManager {
-  static async getProducts() {
-    return RequestManager.fetchData("/products");
-    //  const response = await fetch("http://localhost:3000/api/v1/products");
-    //  const data = await response.json();
-    //  console.log("API Response:", data); // Log the response to check what's returned
-    //  return data;
-  }
-  static async getProductById(id) {
-    return RequestManager.fetchData(`/products/register/${id}`);
-  }
-  static async addProduct(data, id) {
-    return RequestManager.doPostRequest(
-      `/products/register/${id}`,
-      data,
-      "./list.html"
-    );
-  }
-  static async deleteProduct(id) {
-    return RequestManager.deleteRequest("/products", id);
+class ProductsApiManagerClass extends BaseApiManager {
+  async getFiltersData(params) {
+    return RequestManager.fetchData(`${this.routeBase}/filters-data`);
   }
 }
+
+const ProductsApiManager = new ProductsApiManagerClass("/products");
