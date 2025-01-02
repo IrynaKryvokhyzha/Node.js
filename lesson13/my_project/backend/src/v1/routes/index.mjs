95a84ce2f9b82f32adb42a97535883d1174774cc
Iryna <irynazankovska@gmail.com>
2025-01-01 20:21:03 -0500

    made cart and change quantity work

--- lesson13/my_project/backend/src/v1/routes/index.mjs
+++ lesson13/my_project/backend/src/v1/routes/index.mjs
@@ -1,13 +1,15 @@
-import express from 'express'
+import express from "express";
 
-import authRoutes from './authRoutes.mjs'
-import userRoutes from './userRoutes.mjs'
-import productRoutes from './productRoutes.mjs'
+import authRoutes from "./authRoutes.mjs";
+import userRoutes from "./userRoutes.mjs";
+import productRoutes from "./productRoutes.mjs";
+import cartRoutes from "./cartRoutes.mjs";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/auth', authRoutes)
-router.use('/users', userRoutes)
-router.use('/products', productRoutes)
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/products", productRoutes);
+router.use("/cart", cartRoutes);
 
-export default router
+export default router;
